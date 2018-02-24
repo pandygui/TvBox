77e5f9d6a102ac3403ba7efdb58392c77677f695
wangw <wangwei@huuu.com>
2018-02-24 18:32:21 +0800

    Mod:初始提交

---
+++ app/src/main/java/com/wangw/tvbox/activity/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.wangw.tvbox.activity;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by wangw on 2018/2/24.
+ */
+
+public class BaseActivity extends FragmentActivity {
+}
